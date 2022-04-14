a3a98c66aba8b0a1115b074669b9d00006e24dfb
hayeon99 <johayoun@naver.com>
2022-04-14 14:05:12 +0900

    modify-django

--- matchat/views.py
+++ matchat/views.py
@@ -173,9 +173,9 @@ def pay(request, product_id):
             "quantity": "1",                # 구매 물품 수량
             "total_amount": product.price,        # 구매 물품 가격
             "tax_free_amount": "0",         # 구매 물품 비과세
-            "approval_url": "http://127.0.0.1:8000/matchat/pay/approval",
-            "cancel_url": "http://127.0.0.1:8000/matchat/pay/cancel",
-            "fail_url": "http://127.0.0.1:8000/matchat/pay/fail",
+            "approval_url": "http://ec2-3-39-141-76.ap-northeast-2.compute.amazonaws.com/matchat/pay/approval",
+            "cancel_url": "http://ec2-3-39-141-76.ap-northeast-2.compute.amazonaws.com/matchat/pay/cancel",
+            "fail_url": "http://ec2-3-39-141-76.ap-northeast-2.compute.amazonaws.com/matchat/pay/fail",
         }
 
         res = requests.post(URL, headers=headers, params=params)
