fe69099708b1bbda40d50685338a287accff63d3
Soyun Won <soyun9902@ewhain.net>
2022-05-06 19:16:31 +0900

    web에 등록한 사진 detect 하는 코드 주석처리

--- matchat/views.py
+++ matchat/views.py
@@ -121,10 +121,10 @@ def product_create(request):
                 photos.product = product
                 photos.photo = img
                 photos.save()
-                if count == 1:
-                    product.web_result = detect_photo(img)
-                    product.save()
-                    count = 0  # 처음 입력 받은 사진만(=count가 1일 때) detect하기
+                # if count == 1:
+                #     product.web_result = detect_photo(img)
+                #     product.save()
+                #     count = 0  # 처음 입력 받은 사진만(=count가 1일 때) detect하기
             return redirect('matchat:main')
     else:
         form = ProductForm()
