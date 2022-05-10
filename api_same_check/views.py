c1960018144ef9753a5cb1723de13545c42927fa
hayeon99 <johayoun@naver.com>
2022-05-10 18:34:07 +0900

    django-modify

--- api_same_check/views.py
+++ api_same_check/views.py
@@ -18,19 +18,14 @@ class Result(APIView):
         # 유효성 검사
         if serializer.is_valid():
             key = serializer.validated_data['key']  # key에 값 저장
-            #product = Product.objects.get(key=key)  # 해당 key를 가진 상품 호출
-            products = Product.objects.all()
-            count = 0
+            try:
+                product = Product.objects.get(key=key)  # 해당 key를 가진 상품 호출
+            except Product.DoesNotExist:
+                product = None
 
-            for match in products.key:
-                if match == key:
-                    count = 1
-
-            if count == 0:
+            if product == None:
                 data = {'status': '2'}
                 return Response(data)
-
-
 
             # 해당 key를 가진 상품에, 사진 url과 kiosk_result 저장
             product.kiosk_photo = serializer.validated_data['kiosk_photo']
