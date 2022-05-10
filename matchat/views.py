a4bce009688a754c7a0f997516d8e6b30c6bcd03
hayeon99 <johayoun@naver.com>
2022-05-10 12:34:21 +0900

    django-modify

--- matchat/views.py
+++ matchat/views.py
@@ -222,7 +222,7 @@ def pay(request, product_id):
 
 
 def approval(request):
-    product = get.objects.all()
+    product = GET.objects.all()
     # product.reservation = request.user
     # product.save()
     URL = 'https://kapi.kakao.com/v1/payment/approve'
@@ -233,7 +233,7 @@ def approval(request):
     params = {
         "cid": "TC0ONETIME",  # 테스트용 코드
         "tid": request.session['tid'],  # 결제 요청시 세션에 저장한 tid
-        "partner_order_id": '1',      # 주문번호
+        "partner_order_id": product.key,      # 주문번호
         "partner_user_id": request.user.username,    # 유저 아이디
         "pg_token": request.GET.get("pg_token"),  # 쿼리 스트링으로 받은 pg토큰
     }
