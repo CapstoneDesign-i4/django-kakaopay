b3109861feb03ed8dd12e3b0c5086235e0fe7544
hayeon99 <johayoun@naver.com>
2022-05-10 16:29:48 +0900

    django-modify

--- matchat/views.py
+++ matchat/views.py
@@ -206,10 +206,10 @@ def pay(request, product_id):
             "cid": "TC0ONETIME",  # 테스트용 코드
             "partner_order_id": '1',  # 주문번호
             "partner_user_id": request.user.username,  # 유저 아이디
-            "item_name": product.name,  # 구매 물품 이름
+            "item_name": product.key,  # 구매 물품 이름
             "quantity": "1",  # 구매 물품 수량
             "total_amount": product.price,  # 구매 물품 가격
-            "tax_free_amount": product.key,  # 구매 물품 비과세
+            "tax_free_amount": '0',  # 구매 물품 비과세
             "approval_url": "http://ec2-3-39-141-76.ap-northeast-2.compute.amazonaws.com/matchat/pay/approval",
             "cancel_url": "http://ec2-3-39-141-76.ap-northeast-2.compute.amazonaws.com/matchat/pay/cancel",
             "fail_url": "http://ec2-3-39-141-76.ap-northeast-2.compute.amazonaws.com/matchat/pay/fail",
