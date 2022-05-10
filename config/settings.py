d77d194939535aafc41eb07de668ef4577a706e7
hayeon99 <johayoun@naver.com>
2022-05-10 17:49:03 +0900

    django-modify

--- config/settings.py
+++ config/settings.py
@@ -125,8 +125,3 @@ LOGOUT_REDIRECT_URL ='/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ]
-}
