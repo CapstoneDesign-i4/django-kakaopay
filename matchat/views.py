adb810242baa8b44fc1168037000ddef9115189d
hayeon99 <johayoun@naver.com>
2022-05-10 16:12:11 +0900

    django-modify

--- matchat/views.py
+++ matchat/views.py
@@ -204,12 +204,12 @@ def pay(request, product_id):
         }
         params = {
             "cid": "TC0ONETIME",  # 테스트용 코드
-            "partner_order_id": product.key,  # 주문번호
+            "partner_order_id": '1',  # 주문번호
             "partner_user_id": request.user.username,  # 유저 아이디
             "item_name": product.name,  # 구매 물품 이름
             "quantity": "1",  # 구매 물품 수량
             "total_amount": product.price,  # 구매 물품 가격
-            "tax_free_amount": "0",  # 구매 물품 비과세
+            "key": product.key,  # 구매 물품 비과세
             "approval_url": "http://ec2-3-39-141-76.ap-northeast-2.compute.amazonaws.com/matchat/pay/approval",
             "cancel_url": "http://ec2-3-39-141-76.ap-northeast-2.compute.amazonaws.com/matchat/pay/cancel",
             "fail_url": "http://ec2-3-39-141-76.ap-northeast-2.compute.amazonaws.com/matchat/pay/fail",
@@ -230,7 +230,7 @@ def approval(request):
     params = {
         "cid": "TC0ONETIME",  # 테스트용 코드
         "tid": request.session['tid'],  # 결제 요청시 세션에 저장한 tid
-        "partner_order_id": 'partner_order_id',      # 주문번호
+        "partner_order_id": '1',      # 주문번호
         "partner_user_id": request.user.username,    # 유저 아이디
         "pg_token": request.GET.get("pg_token"),  # 쿼리 스트링으로 받은 pg토큰
     }
