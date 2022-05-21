1d131ea29b33958303abdabf56c8cdf4d6a5f71a
Soyun Won <soyun9902@ewhain.net>
2022-05-22 03:14:41 +0900

    s3 url

--- matchat/views.py
+++ matchat/views.py
@@ -110,7 +110,10 @@ def product_create(request):
                 photos.product = product
                 photos.photo = img
                 photos.save()
-                product.web_result = str(img)
+                img_str = str(img)
+                img_url = "https://django-matchat.s3.ap-northeast-2.amazonaws.com/media/" + str(product.author) + "/" + str(product.name) + "/" + img_str
+                res = requests.post("http://ec2-15-164-129-198.ap-northeast-2.compute.amazonaws.com:5000/predict", files={"url": str(img_url)}).json()
+                product.web_result = res[0]['name']
                 product.save()
             return redirect('matchat:main')
     else:
