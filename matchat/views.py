4815e33a0ca6802ee1061fa39b6381c5a0da4a6c
Soyun Won <soyun9902@ewhain.net>
2022-05-21 12:25:01 +0900

    detect

--- matchat/views.py
+++ matchat/views.py
@@ -95,9 +95,9 @@ def my_detail(request, product_id):
     return render(request, 'matchat/my_product_detail.html', context)
 
 
-def detect_photo():
-    image_url = "http://ec2-3-39-141-76.ap-northeast-2.compute.amazonaws.com/media/admin/test444/22.jpg"
-    res = requests.post("http://ec2-15-164-129-198.ap-northeast-2.compute.amazonaws.com:5000/predict", files={"url": image_url}).json()
+def detect_photo(img, product):
+    TEST_IMAGE = "http://ec2-3-39-94-66.ap-northeast-2.compute.amazonaws.com/media/admin/test2/22.jpg"
+    res = requests.post("http://ec2-15-164-129-198.ap-northeast-2.compute.amazonaws.com:5000/predict", files={"url": TEST_IMAGE}).json()
     return res[0]['name']
 
 
@@ -116,7 +116,7 @@ def product_create(request):
                 photos.product = product
                 photos.photo = img
                 photos.save()
-                product.web_result = detect_photo()
+                product.web_result = detect_photo(img, product)
                 product.save()
             return redirect('matchat:main')
     else:
