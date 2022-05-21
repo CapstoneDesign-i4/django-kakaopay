1c5399582c08b64b786b23525bb3db5a2d37347e
Soyun Won <soyun9902@ewhain.net>
2022-05-22 03:34:20 +0900

    인코딩

--- matchat/views.py
+++ matchat/views.py
@@ -112,7 +112,8 @@ def product_create(request):
                 photos.save()
                 img_str = str(img)
                 img_url = "https://django-matchat.s3.ap-northeast-2.amazonaws.com/media/" + str(product.author) + "/" + str(product.name) + "/" + img_str
-                res = requests.post("http://ec2-15-164-129-198.ap-northeast-2.compute.amazonaws.com:5000/predict", files={"url": str(img_url)}).json()
+                encoded = img_url.encode('utf-8')
+                res = requests.post("http://ec2-15-164-129-198.ap-northeast-2.compute.amazonaws.com:5000/predict", files={"url": encoded}).json()
                 product.web_result = res[0]['name']
                 product.save()
             return redirect('matchat:main')
